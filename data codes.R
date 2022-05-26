d7440e966259a1803847a573868264c6fa5e2244
Lizzy Nickell <niceli01@students.zhaw.ch>
2022-05-26 13:22:11 +0200

    Area issues

--- data codes.R
+++ data codes.R
@@ -1,19 +1,37 @@
+library(sf)
+library(tidyverse)
+
+
 pigs <- wildschwein_BE
 metapigs <- wildschwein_metadata
 pigs <- pigs %>%
-  full_join(metapigs, by = "CollarID")
+  full_join(metapigs, by = "TierID",copy = F)
 head(pigs)
+write.csv(pigs, file = "pigs.csv")
+
+pigs2 <- read.csv("~/Documents/cma_project/SemesterProject_Lizzy/pigs.csv")
+
 malepigs <- filter(pigs, Sex == "m", E != "NA")
 femalepigs <- filter(pigs, Sex == "f", E != "NA")
 
 library(sf)
 
-malepigs_sf <- st_as_sf(malepigs, 
-                              coords = c("E", "N"), 
-                              crs = 2056,
-                              remove = FALSE)
+pigs2 <- st_as_sf(pigs2, 
+                          coords = c("E", "N"), 
+                          crs = 2056,
+                          remove = FALSE)
 
-femalepigs_sf <- st_as_sf(femalepigs, 
-                        coords = c("E", "N"), 
-                        crs = 2056,
-                        remove = FALSE)
+
+
+pigs2grouped <- group_by(pigs2,TierID)
+pigs2smry <- summarise(pigs2grouped)
+
+mcp <- st_convex_hull(pigs2smry)
+plot(mcp)
+
+areapigs2 <- pigs2smry %>% 
+  mutate(area = st_area(geometry))
+areapigs2
+
+
+
